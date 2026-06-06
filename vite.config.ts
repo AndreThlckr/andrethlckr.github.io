af08ab21ec8fc20833098343ef1c5026eaa18907
André Theilacker <andre.theilacker@ambevtech.com.br>
2026-06-06 11:45:24 -0300

    updated deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: "/froordle",
+    base: '/froordle/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
